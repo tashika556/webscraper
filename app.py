b630972d46170ec87249ff5ada55eb4fe93b5c39
tashika556 <tashika556@gmail.com>
2025-07-15 22:53:25 -0400

    geturl and response and search certain class

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
+
+url = "http://quotes.toscrape.com"
+response = requests.geturl()
+
+soup = BeautifulSoup(response.text,'html.parser')
+quotes = soup.findall('div',class_='quote')
+
