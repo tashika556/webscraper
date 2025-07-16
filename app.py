6a6491bfd66e0e268d6bd1e69af6309667899ee5
tashika556 <tashika556@gmail.com>
2025-07-15 23:01:07 -0400

    Save file in csv with given data

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ response = requests.geturl()
 soup = BeautifulSoup(response.text,'html.parser')
 quotes = soup.findall('div',class_='quote')
 
+with open('quotesextracted.csv','w', newline='',encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Quote','Author'])
+
+for quote in quotes:
+    text = quote.find('span',class_='text').text.strip()
+    author = quote.find('small',class_='author').text.strip()  
+    writer.writerow([text,author])
+
